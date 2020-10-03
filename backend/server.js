1c196a24198de3afa57753e455b93c446d850eb3
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-10-04 10:50:51 +1300

    add db connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -25,5 +29,6 @@ app.listen(
     5000,
     console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+            .yellow.bold
     )
 );

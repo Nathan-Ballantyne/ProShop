b4821734ea87b45c6858a23bd8b1381e601dc470
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-10-03 17:24:11 +1300

    add routing and product screen template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import HomeScreen from './components/Screens/HomeScreen/HomeScreen';
+import ProductScreen from './components/Screens/ProductScreen/ProductScreen';
 
 const App = () => {
     return (
-        <>
+        <Router>
             <Header />
             <main className='py-3'>
                 <Container>
-                    <HomeScreen />
+                    <Route path='/' component={HomeScreen} exact />
+                    <Route path='/product/:id' component={ProductScreen} />
                 </Container>
             </main>
             <Footer />
-        </>
+        </Router>
     );
 };
 

04e9bb722a370067c2da925143923bfd18c96ad8
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-10-31 14:53:57 +1300

    add login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer/Footer';
 import HomeScreen from './components/Screens/HomeScreen/HomeScreen';
 import ProductScreen from './components/Screens/ProductScreen/ProductScreen';
 import CartScreen from './components/Screens/CartScreen/CartScreen';
+import LoginScreen from './components/Screens/LoginScreen/LoginScreen';
 
 const App = () => {
     return (
@@ -13,9 +14,10 @@ const App = () => {
             <Header />
             <main className='py-3'>
                 <Container>
-                    <Route path='/' component={HomeScreen} exact />
+                    <Route path='/login' component={LoginScreen} />
                     <Route path='/product/:id' component={ProductScreen} />
                     <Route path='/cart/:id?' component={CartScreen} />
+                    <Route path='/' component={HomeScreen} exact />
                 </Container>
             </main>
             <Footer />

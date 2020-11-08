30f754a9bdaaa37e527f087016d6a9d361afe962
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-11-08 14:29:22 +1300

    add space between Address header and details

--- frontend/src/components/Screens/PlaceOrderScreen/PlaceOrderScreen.js
+++ frontend/src/components/Screens/PlaceOrderScreen/PlaceOrderScreen.js
@@ -58,7 +58,7 @@ const PlaceOrderScreen = ({ history }) => {
                         <ListGroup.Item>
                             <h2>Shipping</h2>
                             <p>
-                                <strong>Address:</strong>
+                                <strong>Address: </strong>
                                 {cart.shippingAddress.address},{' '}
                                 {cart.shippingAddress.city},{' '}
                                 {cart.shippingAddress.postalCode},{' '}

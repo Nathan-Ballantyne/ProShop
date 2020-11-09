74a64f96eeef65f93e277983498ef955fd723777
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-11-09 17:09:53 +1300

    change name to match paypal response

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -33,7 +33,7 @@ const orderSchema = mongoose.Schema(
         paymentResult: {
             id: { type: String },
             status: { type: String },
-            updated_time: { type: String },
+            update_time: { type: String },
             email_address: { type: String },
         },
         taxPrice: {

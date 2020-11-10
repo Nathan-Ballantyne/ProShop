8e7b2e050a01b76ee2cf24a386436c9d6a87280b
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-11-11 11:15:33 +1300

    add delete user route and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -107,9 +107,24 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
 });
 
 // @desc   Get all users
-// @route  GET /api/user
+// @route  GET /api/users
 // @access Private/Admin
 export const getUsers = asyncHandler(async (req, res) => {
     const users = await User.find({});
     res.json(users);
+});
+
+// @desc   Delete user
+// @route  DELETE /api/users/:id
+// @access Private/Admin
+export const deleteUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+        await user.remove();
+        res.json({ message: 'User removed' });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });

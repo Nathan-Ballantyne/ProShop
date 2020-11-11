48f184e59940481104ff580015280c1a263f9b14
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-11-12 12:16:43 +1300

    add create and update products route and controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -37,3 +37,56 @@ export const deleteProduct = asyncHandler(async (req, res) => {
         throw new Error('Product not found');
     }
 });
+
+// @desc   Create a product
+// @route  POST /api/products
+// @access Private/Admin
+export const createProduct = asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: 'Sample Name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample Brand',
+        category: 'Sample Category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample Description',
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+});
+
+// @desc   Update a product
+// @route  PUT /api/products/:id
+// @access Private/Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+    const {
+        name,
+        price,
+        image,
+        brand,
+        category,
+        countInStock,
+        description,
+    } = req.body;
+
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        product.name = name;
+        product.price = price;
+        product.image = image;
+        product.brand = brand;
+        product.category = category;
+        product.countInStock = countInStock;
+        product.description = description;
+
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+    } else {
+        res.status(404);
+        throw new Error('Product not found');
+    }
+});

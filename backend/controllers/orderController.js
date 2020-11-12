2fb142e6dd7ddb4796f8bdc6c589e1fdfdb45079
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-11-12 17:06:56 +1300

    add update is delivered route and controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,6 +79,25 @@ export const updateOrderToPaid = asyncHandler(async (req, res) => {
     }
 });
 
+// @desc   Update order to delivered
+// @route  PUT /api/orders/:id/deliver
+// @access Private/Admin
+export const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+        order.isDelivered = true;
+        order.deliveredAt = Date.now();
+
+        const updatedOrder = await order.save();
+
+        res.json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+});
+
 // @desc   Get logged in users orders
 // @route  GET /api/orders/myorders
 // @access Private

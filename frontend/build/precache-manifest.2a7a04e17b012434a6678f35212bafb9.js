a234dbe08a7ea00abe0e0f4d18b8feb77d14c503
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-12-05 16:46:00 +1300

    serve static files when in production

---
+++ frontend/build/precache-manifest.2a7a04e17b012434a6678f35212bafb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b067e0102a07da89f2ddda7ee140cc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d198f4e1d81f0a846a10",
+    "url": "/static/css/main.307a6935.chunk.css"
+  },
+  {
+    "revision": "6a5b04559351eee97602",
+    "url": "/static/js/2.9ecc9453.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9ecc9453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d198f4e1d81f0a846a10",
+    "url": "/static/js/main.a7e20da5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
